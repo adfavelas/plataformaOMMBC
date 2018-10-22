186600ad9a48c256bf9c463690e2db6e60ae8050
Diego <diegocoronado_@outlook.com>
2018-10-22 06:49:07 -0700

    Changed bcrypt to bcrypt-nodejs

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,4 @@
-const bcrypt        = require('bcrypt');
+const bcrypt        = require('bcrypt-nodejs');
 const User          = require('../models/User');
 const jwt           = require('jsonwebtoken');
 const Student       = require('../models/Student');
