d753a65ce80752ffe0dac49f37fee344dff2c231
antoniogodoy17 <tonny-008@hotmail.com>
2018-10-29 09:55:12 -0700

    Sign Up modifed (Date Picker color and language changed). Header modified (Comments deleted)

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -28,12 +28,77 @@ export class SignupComponent implements OnInit {
         this.initForm();
 
         $(document).ready(function() {
-            const options = {
-                'format': 'dd/mm/yyyy'
+            const primaryColorLight = '#0767A4';
+            const secondaryColor = '#0693BE';
+
+            const datePickerOptions = {
+                onDraw: () => {
+                    $('.dropdown-content li>a, .dropdown-content li>span').css('color', secondaryColor);
+                    $('.datepicker-date-display').css('background-color', primaryColorLight);
+                    $('.datepicker-calendar-container .datepicker-footer .confirmation-btns > button').css('color', secondaryColor);
+                    $('.datepicker-table td.is-selected').css('background-color', primaryColorLight);
+                },
+                format: 'dd/mm/yyyy',
+                defaultDate: new Date(2006, 11, 31),
+                minDate: new Date(1900, 0, 1),
+                maxDate: new Date(2006, 11, 31),
+                i18n: {
+                    cancel: 'Cancelar',
+                    done: 'Aceptar',
+                    months: [
+                        'Enero',
+                        'Febrero',
+                        'Marzo',
+                        'Abril',
+                        'Mayo',
+                        'Junio',
+                        'Julio',
+                        'Agosto',
+                        'Septiembre',
+                        'Octubre',
+                        'Noviembre',
+                        'Diciembre'
+                    ],
+                    monthsShort: [
+                        'Ene',
+                        'Feb',
+                        'Mar',
+                        'Abr',
+                        'Mayo',
+                        'Jun',
+                        'Jul',
+                        'Ago',
+                        'Sep',
+                        'Oct',
+                        'Nov',
+                        'Dic'
+                    ],
+                    weekdays: [
+                        'Domingo',
+                        'Lunes',
+                        'Martes',
+                        'Miércoles',
+                        'Jueves',
+                        'Viernes',
+                        'Sábado'
+                    ],
+                    weekdaysShort: [
+                        'Dom',
+                        'Lun',
+                        'Mar',
+                        'Mie',
+                        'Jue',
+                        'Vie',
+                        'Sab'
+                    ],
+                    weekdaysAbbrev: ['D', 'L', 'M', 'Mi', 'J', 'V', 'S']
+                }
             };
-            $('.datepicker').datepicker(options);
-            $('select').formSelect();
+            $('.datepicker').datepicker(datePickerOptions);
+            $('select').formSelect({ classes: 'holis'});
             $('.modal').modal();
+
+            $('.dropdown-content li:not(.disabled) > a, .dropdown-content li:not(.disabled) > span').css('color', secondaryColor);
         });
     }
 
