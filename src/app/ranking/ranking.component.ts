1046ee64f22fed6d8a8a6b168475eceda161add3
antoniogodoy17 <tonny-008@hotmail.com>
2018-11-11 21:06:10 -0800

    Ranking view modified (Displaying all users not ordered)

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentsService } from '../students.service';
 
 @Component({
   selector: 'app-ranking',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ranking.component.sass']
 })
 export class RankingComponent implements OnInit {
+    students;
+    constructor(private studentsService: StudentsService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.studentsService.getUsers().subscribe(res => {
+            if (res.errorCode === 0) {
+                this.students = res.students;
+            }
+        });
+    }
 
 }
