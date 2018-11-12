765f11b3d9db823e811b357e797ebf7fdec83f10
antoniogodoy17 <tonny-008@hotmail.com>
2018-11-11 17:59:31 -0800

    Problem Model Modified

--- backend/models/Problem.js
+++ backend/models/Problem.js
@@ -15,14 +15,14 @@ const mongoose = require("mongoose");
 
 const problemSchema = mongoose.Schema({
     area: {type: String, required: true},
-    tema: {type: String, required: true},
-    tema: {type: String, required: true},
-    problem: {type: String, required: true},
+    topic: {type: String, required: true},
+    level: {type: Number, required: true},
+    problemDescription: {type: String, required: true},
     image: {type: String},
     solution : {type: String, required: true},
     tip: {type: String, required: true},
     origin: {type: String, required: true},
-    number : {type: String, required: true}
+    problemNumber: {type: String, required: true}
 });
 
 module.exports = mongoose.model("Problem", problemSchema);
