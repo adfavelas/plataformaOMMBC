abfe5f8a8f27292b2a735257a252f8988755c228
Diego <diegocoronado_@outlook.com>
2018-12-13 08:46:07 -0800

    Forum Question Create Route

--- backend/controllers/forumFunctions.js
+++ backend/controllers/forumFunctions.js
@@ -1,12 +1,32 @@
-const ForumQuestions = require('../models/ForumQuestion');
+const ForumQuestion = require('../models/ForumQuestion');
 
 exports.getForumQuestions = (req,res)=> {
-    ForumQuestions.find({} , (err, forumQuestions)=> {
+    ForumQuestion.find({} , (err, forumQuestions)=> {
         if (err) {
             console.log(err);
             return res.json({message: 'Ha ocurrido un error', errorCode: 1});
         } else {
             return res.json({message: 'success', errorCode: 0, forumQuestions});
         }
+    });
+}
+
+exports.createForumQuestion = (req,res)=> {
+    const date = Date.now();
+
+    const forumQuestion = new ForumQuestion({
+        title : req.body.title,
+        question: req.body.question,
+        date: date,
+        questionerId: req.user.email,
+        replies: []
+    });
+
+    forumQuestion.save().then( result => {
+        console.log(result);
+        return res.json({message: "success", errorCode: 0 , result});
+    }).catch( err => {
+        console.log(err);
+        return res.json({errorCode: 1, message: "failed"});
     });
 }
