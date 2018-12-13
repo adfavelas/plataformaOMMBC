edfe06635f5394c90e695bc96442604d96300ddb
Antonio Godoy <tonny-008@hotmail.com>
2018-12-13 08:55:24 -0800

    Added forum questions creation functionality

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ForumService } from '../forum.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 declare let $: any;
 
@@ -10,22 +11,56 @@ declare let $: any;
 })
 export class ForumComponent implements OnInit {
   questions;
+  error;
+  form: FormGroup;
   constructor(private forumService: ForumService ) { }
 
   ngOnInit() {
+    this.initForm();
     $(document).ready(function() {
       $('.collapsible').collapsible();
       $('.fixed-action-btn').floatingActionButton();
       $('.tooltipped').tooltip();
-      $('.modal').modal();
+      $('.modal').modal({ dismissible: false });
     });
 
+    this.getForumQuestions();
+
+  }
+
+  initForm(): void {
+    this.form = new FormGroup({
+      title: new FormControl(Validators.required),
+      question: new FormControl(Validators.required)
+    });
+  }
+
+  getForumQuestions(): void {
     this.forumService.getForumQuesiions().subscribe( res => {
       if (res.errorCode === 0) {
         this.questions = res.forumQuestions;
       }
     });
+  }
 
-}
+  buildQuestions(): object {
+    const newQuestion = {
+      title: this.form.controls.title.value,
+      question: this.form.controls.question.value,
+    };
+    return newQuestion;
+  }
+
+  createQuestion(): void {
+    const newQuestion = this.buildQuestion();
+    this.forumService.createForumQuestion(newQuestion).subscribe(res => {
+      if (res.errorCode === 0) {
+        this.getForumQuestions();
+        $('#forumQuestionModal').modal('close');
+      } else {
+        this.error = res.message;
+      }
+    });
+  }
 
 }
