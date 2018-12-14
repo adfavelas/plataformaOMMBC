95e152549381f96153290331c009aca889f4184c
Diego <diegocoronado_@outlook.com>
2018-12-14 08:25:45 -0800

    register teacher

--- backend/controllers/teacher.js
+++ backend/controllers/teacher.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const Teacher = require('../models/Teacher');
-
+const bcrypt = require('bcrypt-nodejs');
 exports.getPendingTeachers = (req, res, next) => {
     User.find({ $and: [{ role: 'teacher' }, { status: 'pending' }] }, (err, users) => {
         if (err) {
@@ -18,5 +18,52 @@ exports.getPendingTeachers = (req, res, next) => {
             });
             return res.json({ message: 'success', teachers: pendingTeachers, errorCode: 0 });
         });
+    });
+}
+
+exports.registerTeacher = (req,res) => {
+    bcrypt.hash(req.body.password,bcrypt.genSaltSync(10), null ,(err, hash) => {   
+        if (err){  
+            console.log(err);
+            return res.json({message: "El correo electrónico que estás registrando ya se encuentra en uso.", errorCode:1});
+        }
+        const user = new User({
+          email: req.body.email,
+          password: hash,
+          status: 'pending',
+          role: req.body.role
+        });
+        
+        user.save().then( result => {
+            console.log(result);
+            const teacher = new Teacher({
+                name: req.body.name,
+                lastName: req.body.lastName,
+                // age: req.body.age,
+                schoolName: req.body.schoolName,
+                state: req.body.state,
+                country: req.body.country,
+                birthDate: req.body.birthDate,
+                email: req.body.email
+            });
+            teacher.save().then( response => {
+                console.log(response);
+                return res.json({message: "Se ha registrado exitosamente ", errorCode: 0})
+                // sendVerificationEmail(user, (message)=>{
+                //     res.json({message, errorCode: 0});
+                // });
+            }).catch( err => {
+                User.deleteOne({_id: result._id},function() {
+                    return res.json({message: "El correo electrónico que estás registrando ya se encuentra en uso.", errorCode: 1});
+                });
+                if (err) {
+                    console.log(err);
+                    console.log('catch student');
+                }
+            });
+        }).catch( err => {
+            console.log(err.email);
+            return res.json({message: "El correo electrónico que estas registrando ya se encuentra en uso.", errorCode: 1});
+        })
     });
 }
--- backend/routes/teacherRoutes.js
+++ backend/routes/teacherRoutes.js
@@ -7,4 +7,7 @@ router.get('/getPendingTeachers', checkToken, (req, res, next) => {
     teacherFunctions.getPendingTeachers(req, res, next);
 });
 
+router.post('/createTeacher', (req,res,next)=> {
+    teacherFunctions.registerTeacher(req,res);
+});
 module.exports = router;
