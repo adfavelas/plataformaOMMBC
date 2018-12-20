714371b6366e28d356d1fb29ed46f55630e611c1
Diego <diegocoronado_@outlook.com>
2018-12-20 06:20:23 -0800

    Changed Answer Model

--- backend/models/Answer.js
+++ backend/models/Answer.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const answerSchema = mongoose.Schema({
     problemId: {type: String, required: true},
-    studentId: {type: String, required: true},
+    studentEmail: {type: String, required: true},
     answer: {type: String, required: true},
     status: {type: String, required: true} //pending | correct | incorrect
 });
