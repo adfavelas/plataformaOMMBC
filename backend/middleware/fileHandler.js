96c401f83605c2ceeab7668cab8f069909cbf1cc
antoniogodoy17 <tonny-008@hotmail.com>
2019-06-18 12:54:51 -0700

    Added problems to backend

--- backend/middleware/fileHandler.js
+++ backend/middleware/fileHandler.js
@@ -4,11 +4,12 @@ const Problem   = require('../models/Problem');
 
 module.exports = (req,res,next) => {
     Converter().fromFile(`./temp/${req.fileName}`).then(async file => {
-        // console.log(typeof(res));
+        console.log(typeof(res));
         try {
             await Problem.collection.insertMany(file);
             next();
         } catch (err) {
+            console.log(err);
             return res.json({errorCode:1 , message: "Something went wrong, please try again later"})
         }
         // Problem.collection.insert(res, (err, docs) => {
--- backend/models/Problem.js
+++ backend/models/Problem.js
@@ -14,16 +14,18 @@
 const mongoose = require("mongoose");
 
 const problemSchema = mongoose.Schema({
+    idOmmbc: {type: String},
     area: {type: String, required: true},
     topic: {type: String, required: true},
     level: {type: Number, required: true},
-    title: {type: String, required: true},
+    // title: {type: String, required: true},
+    problemNumber: {type: Number, required: true},
     problemDescription: {type: String, required: true},
     image: {type: String},
-    solution : {type: String},
     tip: {type: String},
+    solution : {type: String},
     origin: {type: String, required: true},
-    problemNumber: {type: Number, required: true}
+    list: {type: String}
 });
 
 module.exports = mongoose.model("Problem", problemSchema);
